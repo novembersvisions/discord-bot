9e4c55ac0e06eecb732b8b1ae5eab403c6b3e47f
novembersvisions <hprice@carboncopies.org>
2022-03-30 21:31:11 -0400

    update gatz.py

--- gatz.py
+++ gatz.py
@@ -10,7 +10,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-import aiohttp
+import random
 
 bot = Bot("!")
 client = commands.Bot(command_prefix="!")
@@ -20,12 +20,19 @@ async def on_ready():
    print("Ready")
 
 # Generate airplane images
+NumImages = 22
+ImageBaseLink = 'http://hivemindbot.surge.sh/img/'
+ImageExtension = '.jpg'
+ImageLinks = []
+for x in range(NumImages):
+   ImageLink = f'{ImageBaseLink}{x}{ImageExtension}'
+   ImageLinks.append(ImageLink)
+     
 @client.command()
 async def gatz(ctx):
-   async with aiohttp.ClientSession() as session:
-      request = await session.get('https://biriyani.anoram.com/get')
-      planejson = await request.json()
-      embed = discord.Embed(title="airplane", color=discord.Color.red())
-      embed.set_image(url=planejson['link'])
-      await ctx.send(embed=embed)
-      
+   GatzLink = ImageLinks[random.randint(0, len(ImageLinks)-1)]
+   embed = discord.Embed(title="airplane", color=discord.Color.red())
+   embed.set_image(url=GatzLink)
+   await ctx.send(embed=embed)
+
+client.run(TOKEN)
