f957756ae568e331a1896e35030c5f0888c1e194
novembersvisions <hprice@carboncopies.org>
2022-03-30 21:32:44 -0400

    Update help.py

--- help.py
+++ help.py
@@ -24,8 +24,8 @@ async def info(ctx):
     embed = discord.Embed(title="list of commands", color=discord.Color.red())
     embed.add_field(name="!kafka", value="generate a kafka quote", inline=False)
     embed.add_field(name="!bird", value="generate an image of a bird", inline=False)
-    embed.add_field(name="!willow", value="generate an image of a cat along with an ominous quote", inline=False)
     embed.add_field(name="!gatz", value="generate an image of an airplane", inline=False)
+    embed.add_field(name="!willow", value="generate an image of a cat along with an ominous quote", inline=False)
     embed.add_field(name="additional functionality", value="if you say hi to me, i will respond :)", inline=False)
     await ctx.send(embed=embed)
         
